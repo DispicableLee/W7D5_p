382615f01468b21a764625dae7e2ce1b6e01c858
DispicableLee <lee.robert053@gmail.com>
2023-10-06 10:09:28 -0400

    auth

---
+++ RedditClone/app/models/user.rb
@@ -0,0 +1,54 @@
+class User < ApplicationRecord
+        before_validation :ensure_session_token
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+# figvapebr
+    attr_reader :password
+    
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+        if @user && @user.is_password?(password)
+            @user
+        else
+            nil
+        end
+    end
+
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+
+    def reset_session_token!
+        self.session_token = generate_session_token
+        self.save!
+        self.session_token
+    end
+
+
+
+
+    private
+    # def user_params
+    #     params.require(:user).permit(:username, :password)
+    # end
+
+
+    def generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def ensure_session_token
+        self.session_token ||=generate_session_token
+    end
+
+end
